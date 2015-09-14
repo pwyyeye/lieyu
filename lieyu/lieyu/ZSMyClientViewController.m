0e0c97e8be5b726c1758866f7a1d0ea81955b619
xsq <xuebzufo@163.com>
2015-09-14 21:56:31 +0800

    111

---
+++ lieyu/lieyu/ZSMyClientViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZSMyClientViewController.m
+//  lieyu
+//
+//  Created by 薛斯岐 on 15/9/14.
+//  Copyright (c) 2015年 狼族（上海）网络科技有限公司. All rights reserved.
+//
+
+#import "ZSMyClientViewController.h"
+
+@interface ZSMyClientViewController ()
+
+@end
+
+@implementation ZSMyClientViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ lieyu/lieyu/ZSSeatControlViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZSSeatControlViewController.h
+//  lieyu
+//
+//  Created by 薛斯岐 on 15/9/14.
+//  Copyright (c) 2015年 狼族（上海）网络科技有限公司. All rights reserved.
+//
+
+#import "LYBaseViewController.h"
+
+@interface ZSSeatControlViewController : LYBaseViewController
+
+@end
