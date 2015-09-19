361d0b934407ec65f215be66a34ca092f836f2cc
jollywu <jolly.wu@zkteco.com>
2015-09-20 00:53:09 +0800

    添加酒吧详情

---
+++ lieyu/lieyu/Classes/INeedPlay/BeerBar/BeerBarDetail/BeerBarDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BeerBarDetailViewController.h
+//  lieyu
+//
+//  Created by newfly on 9/19/15.
+//  Copyright © 2015 狼族（上海）网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeerBarDetailViewController : UIViewController
+
+@end
