0ceb43488e5ea48c17128c99400c76304be37221
xsq <xuebzufo@163.com>
2015-11-06 14:25:35 +0800

    11

--- lieyu/lieyu/AppDelegate.m
+++ lieyu/lieyu/AppDelegate.m
@@ -260,6 +260,14 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     
     [[RCIMClient sharedRCIMClient] setDeviceToken:token];
 }
+/**
+ * 推送处理4
+ * userInfo内容请参考官网文档
+ */
+- (void)application:(UIApplication *)application
+didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [[NSNotificationCenter defaultCenter] postNotificationName:RECEIVES_MESSAGE object:nil];
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
