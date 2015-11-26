bd9d42cb536276459499b9a96c0a5188356ccbd8
wujie <wujie@lie98.com>
2015-11-26 13:58:59 +0800

    修复手机号码正则表达式

--- lieyu/lieyu/Utilities/MyUtil.m
+++ lieyu/lieyu/Utilities/MyUtil.m
@@ -92,7 +92,7 @@
         
     }
     
-    NSString *regex = @"^((13[0-9])|(147)|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
+    NSString *regex = @"^((13[0-9])|(147)|(15[^4,\\D])|(18[0-9]))\\d{8}$";
     
     NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
     
