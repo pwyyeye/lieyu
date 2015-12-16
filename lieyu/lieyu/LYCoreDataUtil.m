36a9460f18a9613a9903b8eccf158e8a8b1a40ef
pwy <pwy_ndgt@163.com>
2015-12-16 20:24:30 +0800

    完善

--- lieyu/lieyu/LYCoreDataUtil.m
+++ lieyu/lieyu/LYCoreDataUtil.m
@@ -18,7 +18,7 @@
     });
     return instance;
 }
-//保存数据
+#pragma mark 保存数据
 - (BOOL)saveOrUpdateCoreData:(NSString *)entryName withParam:(NSDictionary *)dic andSearchPara:(NSDictionary *)searchDic{
 
     @try {
@@ -43,10 +43,29 @@
             contactInfo= [NSEntityDescription insertNewObjectForEntityForName:entryName  inManagedObjectContext:context];
         }
        
-        //比对数据库字段是否正确－－暂不写
+        //比对数据库字段是否正确
+        NSDictionary *table_attributes=CACHE_TABLE_ATTRIBUTES;
+        NSArray *attributes=[table_attributes objectForKey:entryName];
         
-//        NSDictionary *table_attributes=CACHE_TABLE_ATTRIBUTES;
-//        NSArray *attribute=[table_attributes objectForKey:entryName];
+        if (attributes!=nil&&attributes.count>0) {
+            NSArray *keys=[dic allKeys];
+            for (NSString *key in keys) {
+                BOOL b=NO;
+                for (NSString *attr in attributes) {
+                    if ([attr isEqualToString:key]) {
+                        b=YES
+                    }
+                }
+                if (b) {
+                    [contactInfo setValue:[dic objectForKey:key] forKey:key];
+                }
+            }
+        }else{//不比对数据库
+            NSArray *keys=[dic allKeys];
+            for (NSString *key in keys) {
+                [contactInfo setValue:[dic objectForKey:key] forKey:key];
+            }
+        }
         
         NSArray *keys=[dic allKeys];
         for (NSString *key in keys) {
@@ -62,7 +81,7 @@
     }
     @catch (NSException *exception) {
         NSLog(@"----pass-insertCoreData error:%@---",exception);
-        return YES;
+        return NO;
     }
     @finally {
         
@@ -70,7 +89,7 @@
     
     return YES;
 }
-
+#pragma mark  自定义查询
 - (NSArray *)getCoreData:(NSString *)entryName withPredicate:(NSPredicate *)predicate
 {
     
@@ -86,12 +105,16 @@
         }
         NSError *error;
         NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
-        for (NSManagedObject *teamObject in fetchedObjects) {
-            
-            NSString *teamName = [teamObject valueForKey:@"lyCacheKey"];
-            NSDictionary *teamCity = [teamObject valueForKey:@"lyCacheValue"];
-            NSLog(@"Team info : %@, %@\n", teamName, teamCity);
-        }
+        
+        //查询结果遍历
+//        for (NSManagedObject *teamObject in fetchedObjects) {
+//            
+//            NSString *teamName = [teamObject valueForKey:@"lyCacheKey"];
+//            NSDictionary *teamCity = [teamObject valueForKey:@"lyCacheValue"];
+//            NSLog(@"Team info : %@, %@\n", teamName, teamCity);
+//        }
+        
+        
         return fetchedObjects;
     }
     @catch (NSException *exception) {
@@ -100,6 +123,23 @@
     
 }
 
+#pragma mark （相对）单条件查询
+- (NSArray *)getCoreData:(NSString *)entryName andSearchPara:(NSDictionary *)searchDic{
+    NSArray *array;
+    //判断是否需要查询
+    if (searchDic!=nil && searchDic.count>0) {
+        //查询数据中是否有该对象，若有则更新，否则新增
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:[NSString stringWithFormat:@"%@ == '%@'",[searchDic allKeys].firstObject,[searchDic objectForKey:[searchDic allKeys].firstObject]]];
+        array=[self getCoreData:entryName withPredicate:predicate];
+        
+    }else{
+        array=[self getCoreData:entryName withPredicate:nil];
+    }
+    return array;
+
+}
+
+#pragma mark （相对）单条件删除
 -(void)deleteCoreData:(NSString *)entryName withSearchPara:(NSDictionary *)searchDic{
     @try {
         //获取上下文
