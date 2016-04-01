d1fd00ea17cd39f2938c4200d7d174d9a3edb5b9
pwy <pwy_ndgt@163.com>
2016-04-01 12:02:10 +0800

    1

--- lieyu/lieyu/Utilities/ZSUrl.h
+++ lieyu/lieyu/Utilities/ZSUrl.h
@@ -10,8 +10,8 @@
 #define lieyu_ZSUrl_h
 //服务器
 //
-//#define LY_SERVER @"http://121.40.229.133:8001/lieyu/"
-//#define QINIU_SERVER @"http://121.40.229.133:8001/portal/"
+#define LY_SERVER @"http://121.40.229.133:8001/lieyu/"
+#define QINIU_SERVER @"http://121.40.229.133:8001/portal/"
 
 //#define LY_SERVER @"http://www.lie98.com/lieyu/"
 //#define QINIU_SERVER @"http://www.lie98.com/portal/"
