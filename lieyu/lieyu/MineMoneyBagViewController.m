c66c8b5b66ead0936748059089a2f4b11d7b1d56
TingTing <TingTing@lie98.com>
2016-09-27 14:23:36 +0800

    21

--- lieyu/lieyu/MineMoneyBagViewController.m
+++ lieyu/lieyu/MineMoneyBagViewController.m
@@ -123,16 +123,6 @@
     }
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 #pragma mark - 按钮事件
 - (IBAction)balanceClick:(UIButton *)sender {
     MineBalanceViewController *mineBalanceVC = [[MineBalanceViewController alloc]initWithNibName:@"MineBalanceViewController" bundle:[NSBundle mainBundle]];
