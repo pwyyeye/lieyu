99c3ea6ffa279bb9c4bedaf667df4170947fb0fa
wujie <wujie@lie98.com>
2016-10-17 17:08:54 +0800

    儿童歌

--- lieyu/lieyu/LiveShow/Controllers/LiveShowViewController.m
+++ lieyu/lieyu/LiveShow/Controllers/LiveShowViewController.m
@@ -232,6 +232,7 @@ static NSString *const rcStystemMessageCellIndentifier = @"LYStystemMessageCellI
     __weak typeof(self) weakSelf = self;
     NSDictionary *roomDict = @{@"liveChatId":_roomid,@"1":@"1"};
     //获取直播的stream配置摄像头,请求stream之后将创建开始界面，所以必须传递roomid,此处已经开始配置好了音频和视频，当点击开始直播请求结束后，收到通知才开始推流。
+    //
     [LYFriendsHttpTool getStreamWithParms:roomDict complete:^(NSDictionary *dict) {
         _stream = dict[@"stream"];
         NSString *jsonString = _stream;
@@ -242,8 +243,6 @@ static NSString *const rcStystemMessageCellIndentifier = @"LYStystemMessageCellI
                                                                      error:&err];
         _streamId = streamJSON[@"id"];
         [weakSelf initPLplayer];//初始化摄像头
-        
-        
         
         _registerView = [[[NSBundle mainBundle] loadNibNamed:@"RegisterLiveShowView" owner:weakSelf options:nil] lastObject];
         _registerView.frame = self.view.bounds;
